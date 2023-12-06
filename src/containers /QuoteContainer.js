55aa2ac80cf5e0d72ab8c84f88e2b9b3e33d219e
Sarah Ogunko <sarahogunko@hotmail.com>
2023-12-06 16:58:04 +0000

    added the fetch quotes api

--- src/containers /QuoteContainer.js
+++ src/containers /QuoteContainer.js
@@ -1,18 +1,31 @@
 import QuoteForm from "../components/QuoteForm";
 import QuoteNavigation from "../components/QuoteNavigation";
 import QuoteList from "../components/QuoteList";
-
-
+import { useState, useEffect } from "react";
 
 const QuoteContainer = () => {
+
+    const[quotes, setQuotes]= useState([])
+    
+    
+    const fetchQuotes = async ()=> {
+        const response = await fetch ("https://quote-garden.onrender.com/api/v3/quotes")
+        const data = await response.json();
+        setQuotes(data);
+    }
+
+    useEffect (()=>{
+        fetchQuotes();
+    }, [])
+    
+    
     return (  
         <>
             <QuoteNavigation />
             <QuoteForm />
-            <QuoteList /> 
-
+            <QuoteList quotes={quotes}/> 
         </>
-        
+    
     );
 }
  
