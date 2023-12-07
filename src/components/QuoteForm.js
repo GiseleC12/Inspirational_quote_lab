10c2886996e3c03e62320f0e44bfa1f68efe983f
Gisele Aisaeva <gisel.aisaeva@gmail.com>
2023-12-07 10:24:34 +0000

    Created the structure of the form

--- src/components/QuoteForm.js
+++ src/components/QuoteForm.js
@@ -1,8 +1,31 @@
-const QuoteForm = () => {
+const QuoteForm = ({}) => {
+
+
+    
+    
+    
     return ( 
-        <>
+        <form id="quote-form">
             <h3>Add a new quote</h3>
-        </>
+            <label htmlFor="quote-name">Quotes:</label>
+            <input 
+                id="quote-name"
+                name="name"
+                type="text"
+                placeholder="enter your quote here"
+                />
+            
+            <label htmlFor="author-name">Author Name:</label>
+            <input
+                id="author-name"
+                name="name"
+                type="text"
+                placeholder="enter the author:"
+            />
+
+            <input type="submit" value="Add Quote"/>
+            
+        </form>
             
      );
 }
